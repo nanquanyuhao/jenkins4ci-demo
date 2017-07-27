2d5a8639c90f4137420d0bfe50db63edf04a2d2c
nanquanyuhao <nanquanyuhao@foxmail.com>
2017-07-27 18:00:07 +0800

    重新制作最新稳定版jenkins镜像，基于2.60.2，仅额外下载绿球插件，另外不强制绑定pom文件，具体阅读README.md

--- jenkins2/README.md
+++ jenkins2/README.md
@@ -1,8 +1,8 @@
-# Docker化Jenkins 第二版（基于jenkins:2.60.1）
+# Docker化Jenkins 第二版（基于jenkins:2.60.2）
 
 ## 应用说明
 - 参考《Using Docker》jenkins:1.609.3示例升级
-- 初始化插件安装中仅加入scm-api及greenballs新版本
+- 初始化插件安装中仅加入greenballs新版本
 - 时间设置时区本地化
 
 ## 安装及启动
@@ -14,13 +14,13 @@
 - 方式一.镜像下载
 	- 通过如下命令从DockerHub直接获取镜像
 	```
-	docker pull nanquanyuhao/jenkins2:v2
+	docker pull nanquanyuhao/jenkins2:2.60.2
 	```
 - 方式二.Dockerfile安装
 	- 在项目子目录jenkins下，通过如下命令完成Jenkins容器镜像安装
 	```
 	cd jenkins2
-	docker build -t nanquanyuhao/jenkins2:v2 .
+	docker build -t nanquanyuhao/jenkins2:2.60.2 .
 	```
 ### Jenkins容器启动步骤
 - 给启动脚本赋予执行权限
@@ -32,3 +32,8 @@ chmod +x jenkins_startup.sh
 ./jenkins_startup.sh
 ```
 - 访问所在宿主机的8080端口即可访问Jenkins程序
+
+## 添加Maven自定义配置文件
+- 此配置文件定制了Maven私服及DockerHub的个人账户
+- 将settings.xml文件拷贝至启动后容器的个人目录下
+- Jenkins启动后，配置Maven的个人配置文件为拷贝后文件的绝对路经即可
--- jenkins2/jenkins_startup.sh
+++ jenkins2/jenkins_startup.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if docker ps -a | grep -i jenkins2-environment ; then
-	docker rm -vf jenkins2-environment
+if docker ps -a | grep -i jenkins2.60.2-environment ; then
+	docker rm -vf jenkins2.60.2-environment
 fi
 
-if docker ps -a | grep -i jenkins2-datacontainer ; then
+if docker ps -a | grep -i jenkins2.60.2-datacontainer ; then
 	echo 'Jenkins Data Container Exists'
 else
-	docker run --name jenkins2-datacontainer nanquanyuhao/jenkins2:v2 echo "Jenkins Data Container"
+	docker run --name jenkins2.60.2-datacontainer nanquanyuhao/jenkins2:2.60.2 echo "Jenkins Data Container"
 fi
 
-docker run -d --name jenkins2-environment -p 8080:8080 --volumes-from jenkins2-datacontainer -v /var/run/docker.sock:/var/run/docker.sock nanquanyuhao/jenkins2:v2
+docker run -d --name jenkins2.60.2-environment -p 8080:8080 --volumes-from jenkins2.60.2-datacontainer -v /var/run/docker.sock:/var/run/docker.sock nanquanyuhao/jenkins2:2.60.2
